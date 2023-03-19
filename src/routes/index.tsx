bfe80dc92c249cdea0c2e50f2e6c933ccb3afcdf
Anartz Mugika Ledo <mugan86@gmail.com>
2023-03-19 10:40:58 +0100

    style: code css options add

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,18 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import {
   $translate as t, // <==== Para ejecutar la traducción de una clave
   Speak,
 } from 'qwik-speak';
 
 export const Home = component$(() => {
-  useStyles$(`
-    span {
-      background-color: #d4cfcf
-    }
-  `)
   return (
     <>
       <h1>{t('app.title')}</h1> (1) 
       <p>Coge de la carpeta "app" en el idioma seleccionado y la propiedad <b>title</b></p>
-      <p>Esto hará referencia al fichero que generamos posteriormente en <span>public/i18n/_locale_/app.json</span></p>
+      <p>Esto hará referencia al fichero que generamos posteriormente en <span class="code">public/i18n/_locale_/app.json</span></p>
       <h3>{t('home.intro')}</h3> (2) 
       <p>Coge de la carpeta "home" en el idioma seleccionado y la propiedad <b>intro</b></p>
-      <p>Esto hará referencia al fichero que generamos posteriormente en <span>public/i18n/_locale_/home.json</span></p>
+      <p>Esto hará referencia al fichero que generamos posteriormente en <span class="code">public/i18n/_locale_/home.json</span></p>
     </>
   );
 });
